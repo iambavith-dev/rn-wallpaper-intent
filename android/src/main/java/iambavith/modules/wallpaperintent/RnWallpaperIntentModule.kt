f624220e0072740c0addfcb88b5ca3098b4aa03c
iambavith <iambavith@gmail.com>
2025-09-19 16:39:32 +0530

    added promise

--- android/src/main/java/iambavith/modules/wallpaperintent/RnWallpaperIntentModule.kt
+++ android/src/main/java/iambavith/modules/wallpaperintent/RnWallpaperIntentModule.kt
@@ -18,38 +18,33 @@ class RnWallpaperIntentModule : Module() {
       val activity = appContext.currentActivity ?: throw IllegalStateException("No current activity")
 
       try {
-        Log.d("RnWallpaperIntent", "Called with URL: $urlString")
-
-        // 1. Download file to cache dir
+        
         val file = File(activity.cacheDir, "wallpaper.jpg")
         URL(urlString).openStream().use { input ->
           FileOutputStream(file).use { output ->
             input.copyTo(output)
           }
         }
-        Log.d("RnWallpaperIntent", "File downloaded: ${file.absolutePath}")
-
-        // 2. Wrap in FileProvider URI
+        
         val uri: Uri = FileProvider.getUriForFile(
           activity,
           activity.packageName + ".provider",
           file
         )
-        Log.d("RnWallpaperIntent", "Got URI: $uri")
-
-        // 3. Launch "Set as" chooser
+        
         val intent = Intent(Intent.ACTION_ATTACH_DATA).apply {
           addCategory(Intent.CATEGORY_DEFAULT)
           setDataAndType(uri, "image/*")
           putExtra("mimeType", "image/*")
           addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
         }
-        Log.d("RnWallpaperIntent", "Prepared intent: $intent")
-
+        
         activity.startActivity(Intent.createChooser(intent, "Set as:"))
-        Log.d("RnWallpaperIntent", "Chooser launched")
+        
+        true
+        
       } catch (e: Exception) {
-        Log.e("RnWallpaperIntent", "Error setting wallpaper", e)
+        
         throw e
       }
     }
